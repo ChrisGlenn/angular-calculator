0b3849e16c793b6de9da902d071f410b1fc0c94d
Chris Glenn <topherblair@gmail.com>
2015-09-21 18:49:26 -0600

    almost done

---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+angular.module('angularCalc')
+.controller('mainCtrl', function($scope){
+	$scope.num1 = null;
+	$scope.operator = null;
+	$scope.num2 = null;
+	$scope.result = 0;
+
+	$scope.Calculate = function(){
+		
+	}
+})
