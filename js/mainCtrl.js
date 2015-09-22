120ea620a8ff55a6786fe90e05acc89ede321d21
Chris Glenn <topherblair@gmail.com>
2015-09-21 19:05:48 -0600

    complete

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,6 +6,52 @@ angular.module('angularCalc')
 	$scope.result = 0;
 
 	$scope.Calculate = function(){
-		
+		//Addition
+		if($scope.operator === '+'){
+			if($scope.num1 && $scope.num2){
+				$scope.result = parseInt($scope.num1) + parseInt($scope.num2);
+			}
+			else{
+				alert("Please enter numbers in both fields");
+			}
+		}
+		//Subtraction
+		if($scope.operator === '-'){
+			if($scope.num1 && $scope.num2){
+				$scope.result = parseInt($scope.num1) - parseInt($scope.num2);
+			}
+			else{
+				alert("Please enter numbers in both fields");
+			}
+		}
+		//Multiplication
+		if($scope.operator === '*'){
+			if($scope.num1 && $scope.num2){
+				$scope.result = parseInt($scope.num1) * parseInt($scope.num2);
+			}
+			else{
+				alert("Please enter numbers in both fields");
+			}
+		}
+		//Division
+		if($scope.operator === '/'){
+			if($scope.num1 && $scope.num2){
+				$scope.result = parseInt($scope.num1) / parseInt($scope.num2);
+			}
+			else{
+				alert("Please enter numbers in both fields");
+			}
+		}
+		//Null
+		if(!$scope.operator){
+			alert("Select an operator");
+		}
+	}
+
+	$scope.Clear = function(){
+		$scope.num1 = null;
+		$scope.operator = null;
+		$scope.num2 = null;
+		$scope.result = 0;
 	}
 })
